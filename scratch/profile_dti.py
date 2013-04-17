b9bfb9051d21f74a585c1a21648d2ec220d01608
arokem <arokem@gmail.com>
2013-04-17 13:33:07 -0700

    Fix the profiling script to make more sense.

--- scratch/profile_dti.py
+++ scratch/profile_dti.py
@@ -3,17 +3,16 @@ import dipy.core.gradients as grad
 import dipy.data as dpd
 import dipy.reconst.dti as dti
 
-dd = nib.load(data).get_data()
 data, bvals, bvecs = dpd.get_data('small_25')
+dd = nib.load(data).get_data()
 gtab = grad.gradient_table(bvals, bvecs)
-    
-def func_jac():
-    tm = dti.TensorModel(gtab, fit_method='NLLS', jac=True)
-    tf = tm.fit(dd)
+tm = dti.TensorModel(gtab, fit_method='NLLS')
 
-def func_no_jac():
-    tm = dti.TensorModel(gtab, fit_method='NLLS', jac=False)
+def func_jac():
+    tm.kwargs = dict(jac=True)
     tf = tm.fit(dd)
     
-if __name__=="__main__":
-    func()
+def func_no_jac():
+    tm.kwargs = dict(jac=False)
+    tf = tm.fit(dd)
+    
