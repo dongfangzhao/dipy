655ede5409ed2c760cd09e48f955ebbc115a7db7
arokem <arokem@gmail.com>
2016-06-07 14:00:59 -0700

    RF: Use dtype tricks to find unique values, instead of diff hack.

--- dipy/tracking/utils.py
+++ dipy/tracking/utils.py
@@ -847,7 +847,7 @@ def length(streamlines, affine=None):
     return map(metrics.length, streamlines)
 
 
-def unique_rows(in_array, dtype='f4'):
+def unique_rows(in_array):
     """
     This (quickly) finds the unique rows in an array
 
@@ -866,18 +866,10 @@ def unique_rows(in_array, dtype='f4'):
        Array with the unique rows of the original array.
 
     """
-    # Sort input array
-    order = np.lexsort(in_array.T)
-
-    # Apply sort and compare neighbors
-    x = in_array[order]
-    diff_x = np.ones(len(x), dtype=bool)
-    diff_x[1:] = (x[1:] != x[:-1]).any(-1)
-
-    # Reverse sort and return unique rows
-    un_order = order.argsort()
-    diff_in_array = diff_x[un_order]
-    return in_array[diff_in_array]
+    array_dtype = np.ascontiguousarray(in_array).view(np.dtype((
+                    np.void, in_array.dtype.itemsize * in_array.shape[1])))
+    _, idx = np.unique(array_dtype, return_index=True)
+    return in_array[np.sort(idx)]
 
 
 @_with_initialize
