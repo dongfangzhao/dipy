39d9c4cd29834cac4d719295045d462934d54490
Dongfang Zhao <dzhao8@iit.edu>
2016-07-07 17:25:06 -0700

    testing memmap

---
+++ dipy/tracking/paralife_memmap/hello_memmap.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+from __future__ import print_function
+import numpy as np
+from tempfile import mkdtemp
+import os.path as path
+
+#
+#DFZ: prepare the memmap file
+#
+data = np.arange(12, dtype='float32')
+data.resize((3,4))
+filename = path.join(mkdtemp(), 'newfile.dat')
+print('filename =', filename)
+#DFZ: open a memmap file:
+fp = np.memmap(filename, dtype='float32', mode='w+', shape=(3,4))
+#DFZ: assign the data
+fp[:] = data[:]
+#DFZ: somehow deletion of file pointer flushes it to the disk...
+del fp
+
+#
+#DFZ: load the entire memmap file;
+#    So by default the numpy data is stored as a 1-D array
+#
+newfp = np.memmap(filename, dtype='float32', mode='r')
+print('newfp =', newfp)
+
+#
+#DFZ: load memmap file by offsets
+#
+fpo = np.memmap(filename, dtype='float32', mode='r', offset=16)
+print('fpo =', fpo)
+
+
+print("Done")
