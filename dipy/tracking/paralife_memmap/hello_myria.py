4aa58b8ab86133239d62440e3f2521191e7f2b14
Dongfang Zhao <dzhao@uw.edu>
2016-07-14 16:15:28 -0700

    update myria to local installation

--- dipy/tracking/paralife_memmap/hello_myria.py
+++ dipy/tracking/paralife_memmap/hello_myria.py
@@ -7,9 +7,10 @@ schema = { "columnNames" : ["name", "pages"],
            "columnTypes" : ["STRING_TYPE","LONG_TYPE"] }
 data = """Brave New World,288
 Nineteen Eighty-Four,376
-We,256"""
+We,256
+DFZ,1"""
 
-connection = MyriaConnection(rest_url='http://demo.myria.cs.washington.edu:8753')
+connection = MyriaConnection(rest_url='http://localhost:8753')
 result = connection.upload_file(
     name, schema, data, delimiter=',', overwrite=True)
 
